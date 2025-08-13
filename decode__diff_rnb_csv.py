222ed3a70f243432003c2483992e96953631b51a
jle-jollec <lejolec@gmail.com>
2025-08-13 19:18:46 +0200

    Update decode__diff_rnb_csv.py pour mettre les traitements sous forme de fonctions

--- decode__diff_rnb_csv.py
+++ decode__diff_rnb_csv.py
@@ -6,51 +6,36 @@ import re
 
 """
 ---------------------------------------
-Récupération en local du fichier CSV de Diff RNB 
-à une date donnée en paramètre
+Fonction de Récupération en local du fichier CSV de Diff RNB 
+à une date donnée en paramètre dans le fichier CSV dont le chemin est fourni en paramètres 
+       -- date_since : (format "aaaa-mm-ddThh:mm:ssZ")
+       -- file_out : chemin vers le fichier CSV en local
 ---------------------------------------
 """
+def getDiff_RNB_from_date(date_since, file_out):
+    # date à partir de laquelle on récupère les changements dans le RNB
+    # date_since = "2025-06-15T00:00:00Z"
 
-# date à partir de laquelle on récupère les changements dans le RNB
-date_since = "2025-06-15T00:00:00Z"
+    # URL de l'API RNB qui renvoie un fichier CSV de différentiel
+    url = "http://rnb-api.beta.gouv.fr/api/alpha/buildings/diff/?since="+date_since
 
-# URL de l'API RNB qui renvoie un fichier CSV de différentiel
-url = "http://rnb-api.beta.gouv.fr/api/alpha/buildings/diff/?since="+date_since
+    response = requests.get(url)
 
-response = requests.get(url)
+    # Vérifie que la requête a réussi
+    if response.status_code == 200:
+        # Enregistrement du contenu dans un fichier CSV local
+        with open(file_out, "wb") as f:
+            f.write(response.content)
+        print("Fichier CSV téléchargé avec succès.")
+        return True
+    else:
+        print(f"Erreur lors de la requête : {response.status_code}")
+        return False
 
-# Vérifie que la requête a réussi
-if response.status_code == 200:
-    # Enregistrement du contenu dans un fichier CSV local
-    with open("diff_rnb_courant.csv", "wb") as f:
-        f.write(response.content)
-    print("Fichier CSV téléchargé avec succès.")
-else:
-    print(f"Erreur lors de la requête : {response.status_code}")
 
 """
 ---------------------------------------
 Import brut du fichier CSV de Diff RNB
----------------------------------------
-"""
-# fichier diff à parser
-#diff_rnb = 'diff_rnb_courant.csv'
-diff_rnb = 'diff_2025-01-01T00_00_44.267000+00_00_2025-02-25T13_53_36.233996+00_00.csv'
-
-# Liste reprenant chaque ligne du csv diff sous forme de dictionnaires
-list_batiments_rnb = []
-
-# Parsage du fichier Diff
-with open(diff_rnb, mode='r', encoding='utf-8') as diff:
-    lecteur_csv = csv.DictReader(diff)
-    for ligne in lecteur_csv:
-        list_batiments_rnb.append(dict(ligne))
-
-#for batiment_rnb in list_batiments_rnb:
-#    print(batiment_rnb)
-
-"""
----------------------------------------
 Tri des batiments RNB et export du fichier rnb_last_changes.csv
 ---------------------------------------
 """
@@ -84,32 +69,143 @@ def rnb_get_most_recent(liste_batiments):
         )
         # On prend le plus récent
         if sorted_items:
-            if rnb_id == 'JRWB4B32MJRQ':
-                print(sorted_items)
-                for item in items:
-                    print(extract_start_date(item['sys_period']))
-
             result.append(sorted_items[0])
     
     return result
 
-# tri des batiments par rnb_id croissant et date la plus récente
-list_batiments_rnb_most_recent = rnb_get_most_recent(list_batiments_rnb)
-list_batiments_rnb_sorted = sorted(list_batiments_rnb_most_recent, key=lambda x: x["rnb_id"])
+"""
+---------------------------------------
+Fonction qui parse le fichier CSV fourni en paramètre
+et trie les batiments par id_rnb et version unique la plus récente copiés dans un fichier rnb_last_changes.csv au même emplacement
+     -ENTREE :
+        - file_in : chemin vers le fichier csv à parser
+    - SORTIE :
+        - list_batiments_rnb_sorted :  liste (liste de dictionnaires) triée des batiments RNB
+---------------------------------------
+"""
+def tri_rnb_last_changes(file_in):
+    # fichier diff à parser
+    diff_rnb = file_in
+    #diff_rnb = 'diff_2025-01-01T00_00_44.267000+00_00_2025-02-25T13_53_36.233996+00_00.csv'
 
-# for batiment_rnb in list_batiments_rnb_sorted:
-#      print(batiment_rnb)
+    # Liste reprenant chaque ligne du csv diff sous forme de dictionnaires
+    list_batiments_rnb = []
 
-# Export de la liste triée dans le fichier rnb_last_changes.csv
-csv_rnb_last_changes = "rnb_last_changes.csv"
+    # Parsage du fichier Diff
+    with open(diff_rnb, mode='r', encoding='utf-8') as diff:
+        lecteur_csv = csv.DictReader(diff)
+        for ligne in lecteur_csv:
+            list_batiments_rnb.append(dict(ligne))
 
-# Écriture des données dans un fichier CSV
-with open(csv_rnb_last_changes, mode='w', newline='') as csv_file:
-    fieldnames = list_batiments_rnb_sorted[0].keys()
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    #for batiment_rnb in list_batiments_rnb:
+    #    print(batiment_rnb)
 
-    writer.writeheader()
-    writer.writerows(list_batiments_rnb_sorted)
+    # tri des batiments par rnb_id croissant et date la plus récente
+    list_batiments_rnb_most_recent = rnb_get_most_recent(list_batiments_rnb)
+    list_batiments_rnb_sorted = sorted(list_batiments_rnb_most_recent, key=lambda x: x["rnb_id"])
+
+    # for batiment_rnb in list_batiments_rnb_sorted:
+    #      print(batiment_rnb)
+
+    return list_batiments_rnb_sorted
+
+"""
+---------------------------------------
+Fonction qui parse le fichier CSV fourni en paramètre
+et trie les batiments par id_rnb et version unique la plus récente copiés dans un fichier rnb_last_changes.csv au même emplacement
+     ENTREE :
+        - file_in : chemin vers le fichier csv à parser
+    SORTIE :
+        - csv_rnb_last_changes : chemins vers le fichier csv des batiments RNB triés
+---------------------------------------
+"""
+def tri_rnb_last_changes_csv(file_in):
+    # appel de la fonction de tri
+    list_batiments_rnb_sorted = tri_rnb_last_changes(file_in)
+
+    # Export de la liste triée dans le fichier rnb_last_changes.csv
+    # Enregistrement au même emplacement que le fichier d'entrée
+
+    csv_rnb_last_changes = "rnb_last_changes.csv"
+
+    # Écriture des données dans un fichier CSV
+    with open(csv_rnb_last_changes, mode='w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = list_batiments_rnb_sorted[0].keys()
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(list_batiments_rnb_sorted)
+
+    return csv_rnb_last_changes
+
+
+
+
+"""
+---------------------------------------
+Tri des batiments RNB du fichier rnb_last_changes.csv pour export :
+    ENTREES : 
+        - file_in : chemin vers le fichier csv contenant les "last_changes"
+    SORTIES :
+        - to_remove: liste (set) des identifiants RNB à retirer de la table de liens BDTopo
+        - to_calculate : liste (set) des identifiants RNB dont le lien doit être créé ou mis à jour avec la BDTopo
+---------------------------------------
+"""
+def tri_rnb_to_remove_or_to_calculate(batiments_rnb_last_changes):
+    # création des sets vides
+    to_remove = set()
+    to_calculate = set()
+    batiments_rnb_last_changes_filtred = batiments_rnb_last_changes
+
+    for batiment_rnb in batiments_rnb_last_changes:
+        if (batiment_rnb['is_active']!="1"):
+            # is_active == False, on l'ajoute à to_remove
+            to_remove.add(batiment_rnb['rnb_id'])
+            batiments_rnb_last_changes_filtred.remove(batiment_rnb) #on retire le bati de la liste filtree
+            continue
+
+        if (batiment_rnb['status']=="constructionProject" or batiment_rnb['status']=="canceledConstructionProject"):
+            # valeur du champ status est parmi constructionProject, canceledConstructionProject, on l'ajoute à to_remove
+            to_remove.add(batiment_rnb['rnb_id'])
+            print("rnb to remove : ", batiment_rnb)
+            batiments_rnb_last_changes_filtred.remove(batiment_rnb)  # on retire le bati de la liste filtree
+            continue
+
+        # idenfitiant RNB à conserver pour la MAJ du lien BDTopo
+        to_calculate.add(batiment_rnb['rnb_id'])
+
+    # Export des listes dans les fichier CSV
+    csv_rnb_to_remove = "link_remove.csv"
+    csv_rnb_to_calculate = "link_calculate.csv"
+
+    with open(csv_rnb_to_remove, mode='w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["rnb_id"])
+        for id in to_remove:
+            writer.writerow([id])
+
+    with open(csv_rnb_to_calculate, mode='w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["rnb_id"])
+        for id in to_calculate:
+            writer.writerow([id])
+
+    return batiments_rnb_last_changes_filtred,to_remove,to_calculate
+
+'''
+-----------PARTIE TEST --------------------
+'''
+'''
+# récupération via l'API du fichier de diff
+csv_diff_rnb = "diff_rnb_courant.csv"
+if not getDiff_RNB_from_date("2025-07-15T00:00:00Z",csv_diff_rnb):
+    print("Erreur de récupération du diff. Le programme va s'arrêter")
+    exit()
+
+csv_rnb_last_changes = tri_rnb_last_changes_csv(csv_diff_rnb)
+
+# Liste reprenant chaque ligne de rnb_last_changes.csv sous forme de dictionnaires
+batiments_rnb_last_changes = []
 
 """
 ---------------------------------------
@@ -120,51 +216,27 @@ Tri des batiments RNB du fichier rnb_last_changes.csv pour export dans :
 """
 
 # on repart du fichier last_changes.csv
-csv_rnb_last_changes = "rnb_last_changes.csv"
-
-# Liste reprenant chaque ligne de rnb_last_changes.csv sous forme de dictionnaires
-batiments_rnb_last_changes = []
-
 with open(csv_rnb_last_changes, mode='r', encoding='utf-8') as diff:
     lecteur_csv = csv.DictReader(diff)
     for ligne in lecteur_csv:
         batiments_rnb_last_changes.append(dict(ligne))
 
-
-
-# création des sets vides
-to_remove = set()
-to_calculate = set()
-
-for batiment_rnb in batiments_rnb_last_changes:
-    if (batiment_rnb['is_active']!="1"):
-        # is_active == False, on l'ajoute à to_remove
-        to_remove.add(batiment_rnb['rnb_id'])
-        continue
-
-    if (batiment_rnb['status']=="constructionProject" or batiment_rnb['status']=="canceledConstructionProject"):
-        # valeur du champ status est parmi constructionProject, canceledConstructionProject, on l'ajoute à to_remove
-        to_remove.add(batiment_rnb['rnb_id'])
-        print("rnb to remove : ", batiment_rnb)
-        continue
-
-    # idenfitiant RNB à conserver pour la MAJ du lien BDTopo
-    to_calculate.add(batiment_rnb['rnb_id'])
+rnb_to_remove, rnb_to_calculate = tri_rnb_to_remove_or_to_calculate(batiments_rnb_last_changes)
 
 # Export des listes dans les fichier CSV
 csv_rnb_to_remove = "link_remove.csv"
 csv_rnb_to_calculate = "link_calculate.csv"
 
-with open(csv_rnb_to_remove, mode='w', newline='') as csv_file:
+with open(csv_rnb_to_remove, mode='w', newline='', encoding='utf-8') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(["rnb_id"])
-    for id in to_remove:
+    for id in rnb_to_remove:
         writer.writerow([id])
 
-with open(csv_rnb_to_calculate, mode='w', newline='') as csv_file:
+with open(csv_rnb_to_calculate, mode='w', newline='', encoding='utf-8') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(["rnb_id"])
-    for id in to_calculate:
+    for id in rnb_to_calculate:
         writer.writerow([id])
 
 #     print(batiment_rnb)
@@ -172,3 +244,4 @@ with open(csv_rnb_to_calculate, mode='w', newline='') as csv_file:
 #     print(rnb)
 # for rnb in to_calculate:
 #     print(rnb)
+'''
