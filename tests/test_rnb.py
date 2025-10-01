9bded4a9a51001c57121ec4d48aaa9e7747c3677
pauletienney <p.etienney@gmail.com>
2025-10-01 11:52:13 +0200

    add test

---
+++ tests/test_rnb.py
@@ -0,0 +1,11 @@
+# minimal test class
+import unittest
+
+
+class TestRNBMethods(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
