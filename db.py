836c4d5a9ad803524fb70d1af8d7bf9c870b10e8
pauletienney <p.etienney@gmail.com>
2025-10-29 11:47:29 +0100

    get db params from .env

---
+++ db.py
@@ -0,0 +1,23 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def _get_conn_params() -> dict:
+
+    return {
+        "host": os.getenv("DB_HOST"),
+        "port": os.getenv("DB_PORT"),
+        "user": os.getenv("DB_PORT"),
+        "password": os.getenv("DB_PWD"),
+        "database": os.getenv("DB_NAME"),
+    }
+
+
+def get_cursor():
+
+    params = _get_conn_params()
+
+    pass
