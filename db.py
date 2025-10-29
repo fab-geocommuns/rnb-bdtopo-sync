b0d6b401ddd13b4dff79723357947742675e3def
pauletienney <p.etienney@gmail.com>
2025-10-29 12:05:25 +0100

    test for db params

--- db.py
+++ db.py
@@ -1,11 +1,10 @@
-import psycopg2
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from utils import load_env
 
 
 def _get_conn_params() -> dict:
+
+    load_env()
 
     return {
         "host": os.getenv("DB_HOST"),
