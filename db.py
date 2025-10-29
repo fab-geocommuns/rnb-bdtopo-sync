72cf84d294928490928d41776ee35f9122c38a40
pauletienney <p.etienney@gmail.com>
2025-10-29 11:49:17 +0100

    example env

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ def _get_conn_params() -> dict:
     return {
         "host": os.getenv("DB_HOST"),
         "port": os.getenv("DB_PORT"),
-        "user": os.getenv("DB_PORT"),
+        "user": os.getenv("DB_USER"),
         "password": os.getenv("DB_PWD"),
         "database": os.getenv("DB_NAME"),
     }
